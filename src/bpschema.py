fb42c765d9c7952f46a2ff097a8100418e079911
Poskvansk <luisfilipesrct@gmail.com>
2022-05-04 15:28:27 -0300

    Versao final

--- src/bpschema.py
+++ src/bpschema.py
@@ -1,5 +1,9 @@
 import sqlite3
 
+# TAREFA, EVENTOS, GATEWAYS
+# Tarefa é evento
+# Gateway condicional
+
 def create_tables():
 
     cur.execute( """ CREATE TABLE IF NOT EXISTS Processo (
@@ -66,44 +70,6 @@ def create_tables():
                     FOREIGN KEY (name, next_task1, next_task2) REFERENCES Tarefa (name, name, name),
                     PRIMARY KEY (name)
     );""")
-
-
-
-# def create_relations() :
-
-#     cur.execute( """ CREATE TABLE IF NOT EXISTS next_task (
-
-#                     id_current INT NOT NULL,
-#                     id_next INT NOT NULL,
-
-
-#                     next_task INT NOT NULL,
-#                         FOREIGN KEY (id_current)
-#                             REFERENCES Tarefa (id)
-#                         FOREIGN KEY (id_next)
-#                             REFERENCES Tarefa (id)
-
-#                     PRIMARY KEY(id_current, id_next)
-#     );""")
-
-#     cur.execute( """ CREATE TABLE IF NOT EXISTS next_task_conditional (
-
-#                     id_current INT NOT NULL,
-#                     id_next_if INT NOT NULL,
-#                     id_next_else INT NOT NULL,
-
-
-#                     next_task INT NOT NULL,
-#                         FOREIGN KEY (id_current)
-#                             REFERENCES Tarefa (id)
-#                         FOREIGN KEY (id_next_if)
-#                             REFERENCES Tarefa (id)
-#                         FOREIGN KEY (id_next_else)
-#                             REFERENCES Tarefa (id)
-
-#                     PRIMARY KEY(id_current, id_next_if, id_next_else)
-#     );""")
-
 
 con = sqlite3.connect("bpm.db")
 cur = con.cursor()
